509f2bb1bfc3d08fef3d75ea2579d6d8446525f6
Jose Antonio Diaz Llamas <43948798+SotaDesigner@users.noreply.github.com>
2020-01-27 20:50:57 +0100

    Cofre
    Creación del poryecto de Unity y scrip de cofre, falta empezar a ordenar carpetas y demás

---
+++ Gumplomacy/Assets/ActivarDropCofre.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Con este scrip conseguipos que el cofre detecte a nuestro jugador y asu vez si el jugador pulsa la tecla "e" 
+/// interactuar este activará el booleano y nos dará un item aleatorio.
+/// Hecho por jose antonio 20/01 v.1
+/// </summary>
+[RequireComponent(typeof(Drop))]
+public class ActivarDropCofre : MonoBehaviour
+{
+    bool contactoPlayer;
+
+    Drop activarDrop;
+
+    void Start()
+    {
+        activarDrop = this.GetComponent<Drop>();  
+    }
+    void Update()
+    {
+        if(contactoPlayer && Input.GetButtonDown("Interaztuar"))
+        {
+            activarDrop.drop = true;
+        }
+        else
+        {
+            activarDrop.drop = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            contactoPlayer = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            contactoPlayer = false;
+        }
+    }
+}
