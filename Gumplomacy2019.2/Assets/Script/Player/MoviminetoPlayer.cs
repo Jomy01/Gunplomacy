9676c47a5210fa260b406bf89e3fb3a8706e66de
Jose Antonio Diaz Llamas <43948798+SotaDesigner@users.noreply.github.com>
2020-01-28 20:30:13 +0100

    AnimacionesPlayer

--- Gumplomacy2019.2/Assets/Script/Player/MoviminetoPlayer.cs
+++ Gumplomacy2019.2/Assets/Script/Player/MoviminetoPlayer.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 public class MoviminetoPlayer : MonoBehaviour
 {
     Rigidbody2D mRb;
+    Animator mA;
+    SpriteRenderer mSr;
 
     [Tooltip("Velocidad a la que se moverá el personaje")]
     public float velocidad;
@@ -20,12 +22,34 @@ public class MoviminetoPlayer : MonoBehaviour
     {
         //Llamamos a nuestro rigidbody
         mRb = GetComponent<Rigidbody2D>();
+        mA = GetComponent<Animator>();
+        mSr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        Animacion();
+        FlipPersonaje();
         LeerInput();
         Movimineto(input,velocidad);
+    }
+
+    void Animacion()
+    {
+        if (input.x != 0 || input.y != 0)
+        {
+            mA.SetBool("Andando", true);
+        }
+        else
+        {
+            mA.SetBool("Andando", false);
+        }
+            
+    }
+
+    void FlipPersonaje()
+    {
+        mSr.flipX = (input.x < 0) ?true: false;
     }
     /// <summary>
     /// Recoje la entrade del teclado tanto vertical como horizontal
