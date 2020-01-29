86597c4d596432f7271598a9a6e5c1685483b695
Ignaya <ignaya2@gmail.com>
2020-01-29 20:51:30 +0100

    eter hud añadido
    se ha añadido el hud del eter y los scripts que lo controlan

---
+++ Gumplomacy2019.2/Assets/Script/Generales/GestionEter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestionEter : MonoBehaviour
+{
+    //Slider para el eter de los mutanos
+    Slider sliderMutanos;
+    //Slider para el eter de los mecanos
+     Slider sliderMecanos;
+    //Slider para el eter de los botaniclos
+     Slider sliderBotaniclos;
+    //Slider para el eter de los ictioniclos
+     Slider sliderIctioniclos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderMutanos = GameObject.Find("SliderMutano").GetComponent<Slider>();
+        sliderMecanos = GameObject.Find("SliderMecano").GetComponent<Slider>();
+        sliderBotaniclos = GameObject.Find("SliderBotaniclo").GetComponent<Slider>();
+        sliderIctioniclos = GameObject.Find("SliderIctioniclo").GetComponent<Slider>();
+        sliderMutanos.value = 0;
+        sliderMecanos.value = 0;
+        sliderBotaniclos.value = 0;
+        sliderIctioniclos.value = 0;
+    }
+
+    // Update is called once per frame
+
+    public void ControlEter(int eterValue, string type)
+    {
+        switch(type)
+        {
+            case "EterBotaniclos":
+                sliderBotaniclos.value += eterValue;
+                break;
+            case "EterMutanos":
+                sliderMutanos.value += eterValue;
+                break;
+            case "EterMecanos":
+                sliderMecanos.value += eterValue;
+                break;
+            case "EterIctioniclos":
+                sliderIctioniclos.value += eterValue;
+                break;
+
+        }
+        
+    }
+
+}
