bffb49889eca9602270fc4a267f21be537687521
Jomy01 <43807491+Jomy01@users.noreply.github.com>
2020-01-30 11:20:26 +0100

    menu inicio
    gif, botones y script menu

---
+++ Gumplomacy2019.2/Assets/Script/MenuGP.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuGP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void NuevaPartida()
+    {
+        SceneManager.LoadScene("Partida");
+
+    }
+
+    //¿cómo funciona el sistema de guardado y cómo se llama a la partida salvada?
+    /*public void Continuar()
+    {
+        SceneManager.LoadScene("PartidaSalvada");
+    }
+    */
+
+    public void Opciones()
+    {
+        SceneManager.LoadScene("Opciones");
+
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+
+    }
+
+
+}
