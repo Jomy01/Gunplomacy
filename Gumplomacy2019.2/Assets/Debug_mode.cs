d0926a4af8f0dd6ca5d3947fe811fb27dde7aca0
Ignaya <ignaya2@gmail.com>
2020-02-03 20:53:54 +0100

    empezado mejoras de la nave

---
+++ Gumplomacy2019.2/Assets/Debug_mode.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug_mode : MonoBehaviour
+{
+    int _fuselaje = 0;
+    int _motor = 0;
+    int _navegacion = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("mejoras_fuselaje", _fuselaje);
+        PlayerPrefs.SetInt("mejoras_motor", _motor);
+        PlayerPrefs.SetInt("mejoras_navegacion", _navegacion);
+        PlayerPrefs.SetInt("fuselaje", 0);
+        PlayerPrefs.SetInt("motor", 0);
+        PlayerPrefs.SetInt("navegacion", 0);
+    }
+
+    public void SetMejorasFuselaje()
+    {
+        if (_fuselaje <= 2)
+        {
+            _fuselaje++;
+            PlayerPrefs.SetInt("mejoras_fuselaje", _fuselaje);
+        }
+    }
+
+    public void SetMejorasMotor()
+    {
+        if (_motor <= 2)
+        {
+            _fuselaje++;
+            PlayerPrefs.SetInt("mejoras_motor", _motor);
+        }
+    }
+
+    public void SetMejorasNavegacion()
+    {
+        if (_navegacion <= 2)
+        {
+            _fuselaje++;
+            PlayerPrefs.SetInt("mejoras_navegacion", _navegacion);
+        }
+    }
+
+    public void DeleteAll()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+}
