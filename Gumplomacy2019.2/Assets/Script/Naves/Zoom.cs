accd14a0e10165ac36da90c326286e948c30e174
Cristian <cristian.acu.leal@gmail.com>
2020-02-04 21:01:01 +0100

    #3 Sistema de Navegación Naves en Mapa

--- Gumplomacy2019.2/Assets/Script/Naves/Zoom.cs
+++ Gumplomacy2019.2/Assets/Script/Naves/Zoom.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Zoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 posInicialNave;
+    Vector3 posInicialCam;
+    public Camera Cam;
+    float sizeOriginal;
+    public GameObject cExit;
+    public static bool zoom = false;
+
+    private void Start()
     {
-        
+        posInicialNave = transform.localScale;
+        sizeOriginal = Cam.GetComponent<Camera>().orthographicSize;
+        posInicialCam = Cam.transform.position;
+    }
+    private void OnMouseOver()
+    {
+        if (!zoom) { 
+        transform.localScale = new Vector3(10, 10, 0);
+            
+        }
+      
+    }
+    private void OnMouseExit()
+    {
+        transform.localScale = posInicialNave;
+    }
+    private void OnMouseDown()
+    {
+        cExit.SetActive(true);
+        transform.localScale = posInicialNave;
+        Cam.transform.position = new Vector3(transform.position.x + 1.5f, transform.position.y, -10);
+        Cam.GetComponent<Camera>().orthographicSize = 1.25f;
+        zoom = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Atras()
     {
-        
+
+        zoom = false;
+        Cam.transform.position = posInicialCam;
+        Cam.GetComponent<Camera>().orthographicSize = sizeOriginal;
+        cExit.SetActive(false);
     }
 }
