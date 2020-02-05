71c3727217784df5c9cd82b54ed86afd8fd961af
MariRuiz101 <43808500+MariRuiz101@users.noreply.github.com>
2020-02-05 20:46:01 +0100

    #3 Sistemas Navegacion Naves en Mapa

--- Gumplomacy2019.2/Assets/Script/Naves/Zoom.cs
+++ Gumplomacy2019.2/Assets/Script/Naves/Zoom.cs
@@ -1,27 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Zoom : MonoBehaviour
 {
     Vector3 posInicialNave;
     Vector3 posInicialCam;
-    public Camera Cam;
+    Vector3 posPanel;
+
     float sizeOriginal;
+    public Camera Cam;
+
     public GameObject cExit;
+    public GameObject Panel;
+
     public static bool zoom = false;
+
+    [TextArea]
+    public string textoNave;
+    public Text textoPanel;
 
     private void Start()
     {
         posInicialNave = transform.localScale;
         sizeOriginal = Cam.GetComponent<Camera>().orthographicSize;
         posInicialCam = Cam.transform.position;
+        posPanel = Panel.transform.position;
+
     }
     private void OnMouseOver()
     {
-        if (!zoom) { 
-        transform.localScale = new Vector3(10, 10, 0);
-            
+        if (!zoom)
+        { 
+        transform.localScale = new Vector3(10, 10, 0); 
         }
       
     }
@@ -32,6 +44,10 @@ public class Zoom : MonoBehaviour
     private void OnMouseDown()
     {
         cExit.SetActive(true);
+        Panel.SetActive(true);
+
+        textoPanel.text = textoNave;
+
         transform.localScale = posInicialNave;
         Cam.transform.position = new Vector3(transform.position.x + 1.5f, transform.position.y, -10);
         Cam.GetComponent<Camera>().orthographicSize = 1.25f;
@@ -45,5 +61,7 @@ public class Zoom : MonoBehaviour
         Cam.transform.position = posInicialCam;
         Cam.GetComponent<Camera>().orthographicSize = sizeOriginal;
         cExit.SetActive(false);
+        Panel.SetActive(false);
+
     }
 }
