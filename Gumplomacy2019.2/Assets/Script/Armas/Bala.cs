cda85fef5e30ea974abea6e3cb4e426264906381
Jose Antonio Diaz Llamas <43948798+SotaDesigner@users.noreply.github.com>
2020-02-07 17:57:09 +0100

    #277PreparadoParaLuner

--- Gumplomacy2019.2/Assets/Script/Armas/Bala.cs
+++ Gumplomacy2019.2/Assets/Script/Armas/Bala.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bala : MonoBehaviour
 {
     public float velocidad = 20;
-
+    public int daño;
     Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
---
+++ Gumplomacy2019.2/Assets/VidaProta.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VidaProta : MonoBehaviour
+{
+    int vidas = 6;
+    public Sprite[] spriteVidas;
+    public Image imagenVida;
+
+    private void Start()
+    {
+        imagenVida.sprite = spriteVidas[vidas];
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bala"))
+        {
+            int dañoBala = collision.gameObject.GetComponent<Bala>().daño;
+            vidas = vidas - dañoBala; ;
+            imagenVida.sprite = spriteVidas[vidas];
+            if(vidas <= 0)
+            {
+                PausaEscena();
+                //Muerte();
+            }
+        }
+        if(collision.gameObject.CompareTag("Enemigo"))
+        {
+            vidas--;
+            imagenVida.sprite = spriteVidas[vidas];
+            if (vidas <= 0)
+            {
+                PausaEscena();
+                Muerte();
+            }
+        }
+    }
+    void PausaEscena()
+    {
+        Time.timeScale = 0;
+    }
+    void Muerte()
+    {
+        Destroy(gameObject);
+    }
+}
