fc1865508f46f8e96efb456a431d2519ebd05069
Jose Miguel <jose.miguel.db.17894@gmail.com>
2020-05-13 20:31:29 +0200

    UPDATE vida del jugador

---
+++ Assets/Script/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 6;
+    private int currentHealth = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void SetDamage(int damage)
+    {
+        currentHealth -= damage;
+        HealthIndicator.Instance.UpdateVidaUI(currentHealth);
+        CheckDeath();
+    }
+
+    public void SetHeal(int heal)
+    {
+        currentHealth += heal;
+    }
+
+    public int GetHeal()
+    {
+        return currentHealth;
+    }
+
+    void CheckDeath()
+    {
+        if (currentHealth <= 0)
+        {
+            GameOverMenu.isGameOver = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
